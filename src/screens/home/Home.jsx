e78be9d82fb84c87778aa4580db16fe319c2d3af
guilhermemgsilva <guilherme.alberto.machado@gmail.com>
2021-10-26 19:57:55 -0300

    Adicionando o component MainHome na Home screen

--- src/screens/home/Home.jsx
+++ src/screens/home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Footer from '../../components/footer/Footer'
+import MainHome from '../../components/mainHome/MainHome'
 import Topbar from '../../components/topbar/Topbar'
 
 function Home() {
   return (
     <>
       <Topbar />
+      <MainHome />
       <Footer />
     </>
   )
