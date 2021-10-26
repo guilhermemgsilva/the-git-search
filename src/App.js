bb614dcf65514a238c3693f435b5660fdba8282f
guilhermemgsilva <guilherme.alberto.machado@gmail.com>
2021-10-26 20:39:28 -0300

    Adicionando rect-router-dom no App.js e configurando as rotas

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import Home from './screens/home/Home'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Results from './screens/results/Results'
 
 function App() {
-  return <Home />
+  return (
+    <>
+      <Router>
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+
+          <Route path='/results'>
+            <Results />
+          </Route>
+        </Switch>
+      </Router>
+    </>
+  )
 }
 
 export default App
--- src/screens/results/Results.jsx
+++ src/screens/results/Results.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import Topbar from '../../components/topbar/Topbar'
 import Footer from '../../components/footer/Footer'
-import MainResults from '../../components/mainResults/MainResults'
 
 function Results() {
   return (
     <div>
       <Topbar />
-      <MainResults />
       <Footer />
     </div>
   )
