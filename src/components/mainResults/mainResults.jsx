b39ce859e90e1618440368d5a7b03a064f3367e6
guilhermemgsilva <guilherme.alberto.machado@gmail.com>
2021-10-26 20:44:03 -0300

    Configurando o HTML do component MainResults, onde serão exibidos os resultados das buscas

---
+++ src/components/mainResults/mainResults.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './mainResults.css'
+
+function MainResults() {
+  return (
+    <div className='main'>
+      <div className='results-title'>
+        <p></p>
+      </div>
+
+      <div className='results'></div>
+      <div className='background-image'></div>
+    </div>
+  )
+}
+
+export default MainResults
