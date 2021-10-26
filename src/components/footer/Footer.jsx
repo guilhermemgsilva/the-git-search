98882a998656b42e08b192cf5497b4219d4785a1
guilhermemgsilva <guilherme.alberto.machado@gmail.com>
2021-10-26 19:20:07 -0300

    Criando a estrutura HTML do footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='footer'>
+      <p>Projetado por: Guilherme Machado - 26/10/2021</p>
+    </div>
+  )
+}
+
+export default Footer
